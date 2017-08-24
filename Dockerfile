266b2e6baeebc428e6758a7f8aa92b2182817b46
Maxime Allanic <mallanic@MacBook-Pro-de-Maxime.local>
2017-08-24 18:06:31 +0200

    [FIX] File

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,19 @@
 FROM python:slim
 
-RUN pip install --upgrade --no-cache-dir awsebcli
-
 RUN apt-get update
-RUN apt-get install git -y
+RUN apt-get install git curl -y
 
+# AWS EB
+RUN pip install --upgrade --no-cache-dir awsebcli awscli
+
+# AWS ECS
+RUN curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest
+RUN chmod +x /usr/local/bin/ecs-cli
+
+# Install ENTRYPOINT
 ADD entrypoint.sh /usr/bin/entrypoint
-
 RUN chmod +x /usr/bin/entrypoint
+
+WORKDIR "/tmp"
 
 ENTRYPOINT ["entrypoint"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+## AWS EB
 mkdir -p ~/.aws/
-printf "[eb-cli]\naws_access_key_id = %s\naws_secret_access_key = %s\n" "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY" > ~/.aws/credentials
+printf "[eb-cli]\naws_access_key_id = %s\naws_secret_access_key = %s\n" ${AWS_KEY} ${AWS_SECRET} > ~/.aws/credentials
 printf "[profile eb-cli]\nregion=eu-west-1\noutput=json" > ~/.aws/config
+
+## AWS ECS
+if [ ! -z "${AWS_ECS_CLUSTER}" ]
+ then
+    ecs-cli configure --access-key ${AWS_KEY} --secret-key ${AWS_SECRET} --cluster ${AWS_ECS_CLUSTER} --region eu-west-1
+fi
 
 exec "${@}"
 
