42e6c500aeccd2c0b9ae7007bae36d70830d4db1
Maxime Allanic <mallanic@MacBook-Pro-de-Maxime.local>
2017-09-13 14:55:18 +0200

    [FIX] Docker launch

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# if we have "--link some-docker:docker" and not DOCKER_HOST, let's set DOCKER_HOST automatically
+if [ -z "$DOCKER_HOST" -a "$DOCKER_PORT_2375_TCP" ]; then
+	export DOCKER_HOST='tcp://docker:2375'
+fi
+
 ## AWS EB
 mkdir -p ~/.aws/
 printf "[eb-cli]\naws_access_key_id = %s\naws_secret_access_key = %s\n" ${AWS_KEY} ${AWS_SECRET} > ~/.aws/credentials
